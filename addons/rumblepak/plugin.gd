83f9bdf6c39e41c45a8394d74621488f70abbd58
Elliptical <70788102+cellos51@users.noreply.github.com>
2026-06-15 10:44:41 -0400

    v1.1

--- addons/rumblepak/plugin.gd
+++ addons/rumblepak/plugin.gd
@@ -2,9 +2,14 @@
 extends EditorPlugin
 
 
+const RumbleInspector := preload("res://addons/rumblepak/rumble_inspector.gd")
+var rumble_inspector := RumbleInspector.new()
+
 func _enter_tree() -> void:
-	add_autoload_singleton("RumblePak", "res://addons/rumblepak/rumble_pak.gd")
+	add_inspector_plugin(rumble_inspector)
+	add_autoload_singleton("RumblePak", "res://addons/rumblepak/rumblepak.gd")
 
 
 func _exit_tree() -> void:
+	remove_inspector_plugin(rumble_inspector)
 	remove_autoload_singleton("RumblePak")
--- addons/rumblepak/rumble_preset.gd
+++ addons/rumblepak/rumble_preset.gd
@@ -1,7 +1,9 @@
+@tool
 @icon("res://addons/rumblepak/icons/controller.svg")
 class_name RumblePreset
 extends Resource
 
+@export_tool_button("Test Rumble", "Noise") var test_action := _test
 ## The rumble bus to use when applying controller rumble.
 @export var bus: StringName = &"master"
 ## The intensity of rumble for the preset to be limited to.
@@ -9,6 +11,14 @@ extends Resource
 ## The amount of time in seconds for the preset to last.
 @export var duration: float = 1.0
 ## The curve to be used for the weak motor of the controller.
-@export var weak_curve: Curve
+
 ## The curve to be used for the strong motor of the controller.
+
+@export_group("Weak Rumble Graph")
+@export var weak_curve: Curve
+@export_group("Strong Rumble Graph")
 @export var strong_curve: Curve
+
+
+func _test() -> void:
+	RumblePak.add_rumble(self)
