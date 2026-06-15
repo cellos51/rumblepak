c4c64598da4be0cc220d08000e9ef21e139aa3a8
Elliptical <70788102+cellos51@users.noreply.github.com>
2026-06-14 22:52:44 -0400

    Fix path in add_autoload_singleton for RumblePak

--- addons/rumblepak/plugin.gd
+++ addons/rumblepak/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_autoload_singleton("RumblePak", "res://addons/rumble_pak/rumble_pak.gd")
+	add_autoload_singleton("RumblePak", "res://addons/rumblepak/rumble_pak.gd")
 
 
 func _exit_tree() -> void:
